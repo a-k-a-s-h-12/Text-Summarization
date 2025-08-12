7b80d79a9f4847ee4470d48a2bd6445250ace96b
a-k-a-s-h-12 <90305231+a-k-a-s-h-12@users.noreply.github.com>
2025-08-12 11:54:17 +0530

    Update Register.jsx

--- Frontend/src/pages/Register.jsx
+++ Frontend/src/pages/Register.jsx
@@ -14,7 +14,7 @@ function Register() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      await axios.post("http://localhost:5000/auth/register", formData);
+      await axios.post("https://text-summarization-backend.onrender.com/auth/register", formData);
       alert("Successfully Registered");
       navigate("/login");
     } catch (error) {
