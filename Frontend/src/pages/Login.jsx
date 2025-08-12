ff7f292c8373040d170d99ccd42db2a11c14ce3c
a-k-a-s-h-12 <90305231+a-k-a-s-h-12@users.noreply.github.com>
2025-08-12 11:54:45 +0530

    Update Login.jsx

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -13,7 +13,7 @@ function Login() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post("http://localhost:5000/auth/login", formData);
+      const res = await axios.post("https://text-summarization-backend.onrender.com/auth/login", formData);
 
       // Save token & user ID in localStorage
       localStorage.setItem("token", res.data.token);
